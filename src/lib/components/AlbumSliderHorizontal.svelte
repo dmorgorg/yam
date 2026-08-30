bd9479ffa0f3f93d526d785615ac64f094859d6c
Dave Morgan <dave@dmorg.org>
2026-08-30 17:02:20 -0600

    first 10

---
+++ src/lib/components/AlbumSliderHorizontal.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { fade } from 'svelte/transition';
+	let { hIndex, vIndex, imagesArray } = $props();
+	let count = imagesArray.length;
+</script>
+
+{#key `${hIndex}-${vIndex}`}
+	<div class="wrapper" out:fade={{ duration: 500 }} in:fade={{ duration: 1000, delay: 1000 }}>
+		<!-- <div class="media" bind:this={mediaEl}> -->
+		<enhanced:img
+			src={imagesArray[Number(hIndex)][Number(vIndex)].image}
+			sizes="(min-width:1920px) 1800px, (min-width:1080px) 1080px, (min-width:768px) 768px"
+			alt=""
+		/>
+		<div class="caption">
+			({hIndex + 1}/{count})
+			{@html imagesArray[Number(hIndex)][vIndex].caption}
+		</div>
+	</div>
+{/key}
+
+<!-- </div> -->
+
+<style lang="scss">
+	.wrapper {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: start;
+		width: 80%;
+		margin-inline: auto;
+		max-height: 80vh;
+		padding-top: 0;
+		overflow: visible;
+	}
+
+	enhanced\:img {
+		border: 0.225vw solid black;
+		box-shadow: 0 0 0.5em 0.15em grey;
+		height: auto;
+		margin-inline: auto;
+		max-height: 70vh;
+		max-width: 100%;
+		object-fit: contain;
+		width: auto;
+		overflow-y: auto;
+	}
+	.caption {
+		font-family: 'text';
+		font-size: clamp(10px, 1.25vw, 16px);
+		line-height: 1.2;
+		margin-top: 0.5em;
+		text-align: center;
+		max-width: 90%;
+	}
+</style>
--- src/routes/thelisters/+page.svelte
+++ src/routes/thelisters/+page.svelte
@@ -1,27 +1,104 @@
 <script module>
 	// grebes
 
-	import f05 from '$lib/images/listers/Elopement-1004.jpg?enhanced';
+	import elopement_1004 from '$lib/images/listers/Elopement-1004.jpg?enhanced';
+	import elopement_1008 from '$lib/images/listers/Elopement-1008.jpg?enhanced';
+	import elopement_1011 from '$lib/images/listers/Elopement-1011.jpg?enhanced';
+	import elopement_1016 from '$lib/images/listers/Elopement-1016.jpg?enhanced';
+	import elopement_1020 from '$lib/images/listers/Elopement-1020.jpg?enhanced';
+	import elopement_1024 from '$lib/images/listers/Elopement-1024.jpg?enhanced';
+	import elopement_1027 from '$lib/images/listers/Elopement-1027.jpg?enhanced';
+	import elopement_1034 from '$lib/images/listers/Elopement-1034.jpg?enhanced';
+	import elopement_1049 from '$lib/images/listers/Elopement-1049.jpg?enhanced';
+	import elopement_1056 from '$lib/images/listers/Elopement-1056.jpg?enhanced';
+	import elopement_1058 from '$lib/images/listers/Elopement-1058.jpg?enhanced';
+	import elopement_2107 from '$lib/images/listers/Elopement-2107.jpg?enhanced';
 
 	export const imagesArray = [
 		[
 			{
-				image: f05,
+				image: elopement_1004,
 				caption: 'Elopement-1004.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1008,
+				caption: 'Elopement-1008.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1011,
+				caption: 'Elopement-1011.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1016,
+				caption: 'Elopement-1016.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1020,
+				caption: 'Elopement-1020.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1024,
+				caption: 'Elopement-1024.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1027,
+				caption: 'Elopement-1027.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1034,
+				caption: 'Elopement-1034.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1049,
+				caption: 'Elopement-1049.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1056,
+				caption: 'Elopement-1056.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1058,
+				caption: 'Elopement-1058.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_2107,
+				caption: 'Elopement-2107.jpg'
 			}
 		]
 	];
 </script>
 
 <script>
-	import AlbumSlider3 from '$lib/components/AlbumSlider3.svelte';
+	import AlbumSliderHorizontal from '$lib/components/AlbumSliderHorizontal.svelte';
 	import NavigationController from '$lib/components/NavigationController.svelte';
 	import HomeIcon from '$lib/components/HomeIcon.svelte';
 	import Spacer from '$lib/components/Spacer.svelte';
 	// import BirdsDropDown from '$lib/components/BirdsDropDown.svelte';
 	// import { count } from '$lib/data/counts.svelte.js';
 
-	let title = "Andrea and Peter's Day";
+	let title = "Andrea and Peter's Special Day";
 	let hIndex = $state(0);
 	let vIndex = $state(0);
 </script>
@@ -34,7 +111,9 @@
 		<!-- <BirdsDropDown /> -->
 	</div>
 
-	<div class="slider-container"><AlbumSlider3 bind:hIndex bind:vIndex {imagesArray} /></div>
+	<div class="slider-container">
+		<AlbumSliderHorizontal bind:hIndex bind:vIndex {imagesArray} />
+	</div>
 </div>
 
 <div class="nav-controller">
