b21e6849b7f730d6420c4aaea40cc7cef94c0e4e
Dave Morgan <dave@dmorg.org>
2026-08-30 23:20:03 -0600

    five images

--- src/routes/thelisters/+page.svelte
+++ src/routes/thelisters/+page.svelte
@@ -440,36 +440,36 @@
 				caption: 'Elopement-1357.jpg'
 			}
 		],
-		// [
-		// 	{
-		// 		image: elopement_1359,
-		// 		caption: 'Elopement-1359.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1368,
-		// 		caption: 'Elopement-1368.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1378,
-		// 		caption: 'Elopement-1378.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1382,
-		// 		caption: 'Elopement-1382.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1385,
-		// 		caption: 'Elopement-1385.jpg'
-		// 	}
-		// ],
+		[
+			{
+				image: elopement_1359,
+				caption: 'Elopement-1359.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1368,
+				caption: 'Elopement-1368.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1378,
+				caption: 'Elopement-1378.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1382,
+				caption: 'Elopement-1382.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1385,
+				caption: 'Elopement-1385.jpg'
+			}
+		],
 		// [
 		// 	{
 		// 		image: elopement_1386,
