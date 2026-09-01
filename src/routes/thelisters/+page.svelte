09ef1b40f0436481e5c42dfe7729aec83999b2fd
Dave Morgan <dave@dmorg.org>
2026-09-01 00:21:56 -0600

    post some images, giving up on importing for today

--- src/routes/thelisters/+page.svelte
+++ src/routes/thelisters/+page.svelte
@@ -137,7 +137,7 @@
 	import elopement_1938 from '$lib/images/listers/Elopement-1938.jpg?enhanced';
 	import elopement_1941 from '$lib/images/listers/Elopement-1941.jpg?enhanced';
 	// import elopement_1944 from '$lib/images/listers/Elopement-1944.jpg?enhanced';
-	import elopement_1948 from '$lib/images/listers/Elopement-1948.jpg?enhanced';
+	// import elopement_1948 from '$lib/images/listers/Elopement-1948.jpg?enhanced';
 	// import elopement_1954 from '$lib/images/listers/Elopement-1954.jpg?enhanced';
 	// import elopement_1956 from '$lib/images/listers/Elopement-1956.jpg?enhanced';
 	// import elopement_1964 from '$lib/images/listers/Elopement-1964.jpg?enhanced';
@@ -914,72 +914,72 @@
 				caption: 'Elopement-1875.jpg'
 			}
 		],
-		// [
-		// 	{
-		// 		image: elopement_1897,
-		// 		caption: 'Elopement-1897.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1901,
-		// 		caption: 'Elopement-1901.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1907,
-		// 		caption: 'Elopement-1907.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1912,
-		// 		caption: 'Elopement-1912.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1914,
-		// 		caption: 'Elopement-1914.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1920,
-		// 		caption: 'Elopement-1920.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1930,
-		// 		caption: 'Elopement-1930.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1932,
-		// 		caption: 'Elopement-1932.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1934,
-		// 		caption: 'Elopement-1934.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1938,
-		// 		caption: 'Elopement-1938.jpg'
-		// 	}
-		// ],
-		// [
-		// 	{
-		// 		image: elopement_1941,
-		// 		caption: 'Elopement-1941.jpg'
-		// 	}
-		// ],
+		[
+			{
+				image: elopement_1897,
+				caption: 'Elopement-1897.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1901,
+				caption: 'Elopement-1901.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1907,
+				caption: 'Elopement-1907.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1912,
+				caption: 'Elopement-1912.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1914,
+				caption: 'Elopement-1914.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1920,
+				caption: 'Elopement-1920.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1930,
+				caption: 'Elopement-1930.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1932,
+				caption: 'Elopement-1932.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1934,
+				caption: 'Elopement-1934.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1938,
+				caption: 'Elopement-1938.jpg'
+			}
+		],
+		[
+			{
+				image: elopement_1941,
+				caption: 'Elopement-1941.jpg'
+			}
+		],
 		// [
 		// 	{
 		// 		image: elopement_1944,
